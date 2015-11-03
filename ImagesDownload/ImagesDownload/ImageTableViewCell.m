9c7fb192649300e530bb60fb1a802abab3470de1
Juliana Cipa <juliana.cipa@rnfdigital.com>
2015-11-03 03:09:41 +0000

    Animation should show only if the image is not cached

--- ImagesDownload/ImagesDownload/ImageTableViewCell.m
+++ ImagesDownload/ImagesDownload/ImageTableViewCell.m
@@ -29,12 +29,14 @@ static NSString *const GET_PHOTO_URL = @"http://challenge.superfling.com/photos/
     [self.displayImageView sd_setImageWithURL:getPhotoURL
                              placeholderImage:[UIImage imageNamed:@"download"]
                                     completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-                                        self.displayImageView.alpha = 0.0;
-                                        self.displayImageView.transform =CGAffineTransformMakeScale(0.8, 0.8);
-                                        [UIView animateWithDuration:1.0 animations:^{
-                                            self.displayImageView.alpha = 1.0;
-                                            self.displayImageView.transform =CGAffineTransformMakeScale(1.0, 1.0);
-                                        }];
+                                        if (cacheType == SDImageCacheTypeNone) {
+                                            self.displayImageView.alpha = 0.0;
+                                            self.displayImageView.transform =CGAffineTransformMakeScale(0.8, 0.8);
+                                            [UIView animateWithDuration:1.0 animations:^{
+                                                self.displayImageView.alpha = 1.0;
+                                                self.displayImageView.transform =CGAffineTransformMakeScale(1.0, 1.0);
+                                            }];
+                                        }
                                     }];
 }
 
